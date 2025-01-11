d747182883d60270c85d102fef41ac162b37dc85
Botir Ismatov <botirismatov@MacBook-Pro-Botir.local>
2025-01-11 08:52:09 +0500

    secound commit and upload

---
+++ database/migrations/2025_01_10_135514_create_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->string('id')->unique();
+            $table->string('name');
+            //$table->string('art')->nullable();
+            $table->string('gtin')->nullable();
+            $table->string('img')->nullable();
+            $table->text('desc')->nullable();
+            $table->double('price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
